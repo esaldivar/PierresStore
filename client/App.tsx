a5f48900317cf03929813f1ae90c2219dbec99c6
Eric Saldivar <Eric@samlas-mbp.lan>
2021-07-22 15:14:01 -0500

    eslint configuration complete

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,11 @@
-import './styles.css';
-import IMAGE from './assets/Pierres_shop.png'
+import "./styles.css";
+import IMAGE from "./assets/Pierres_shop.png";
 
 export const App = () => {
-	return <>
-	<h1>React Typescript Webpack Starter Template</h1>
-	<img src={IMAGE} alt="Pierre's Store" width="300" height="200"/>
-	</>
-}
+  return (
+    <>
+      <h1>React Typescript Webpack Starter Template</h1>
+      <img src={IMAGE} alt="Pierre's Store" width="300" height="200" />
+    </>
+  );
+};
