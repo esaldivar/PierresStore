bc00349cf3f9852c1b7d2b0bb300835c008d2030
Eric Saldivar <Eric@samlas-mbp.lan>
2021-07-22 14:49:07 -0500

    able to render images

--- client/App.tsx
+++ client/App.tsx
@@ -1,3 +1,9 @@
+import './styles.css';
+import IMAGE from './assets/Pierres_shop.png'
+
 export const App = () => {
-	return <h1>React Typescript Webpack Starter Template</h1>
+	return <>
+	<h1>React Typescript Webpack Starter Template</h1>
+	<img src={IMAGE} alt="Pierre's Store" width="300" height="200"/>
+	</>
 }
