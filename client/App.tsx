bb98df6b9fa302bf0112bcc59107d09885a8261c
Eric Saldivar <Eric@samlas-mbp.lan>
2021-07-22 15:20:43 -0500

    prettier config is complete

--- client/App.tsx
+++ client/App.tsx
@@ -1,5 +1,5 @@
-import "./styles.css";
-import IMAGE from "./assets/Pierres_shop.png";
+import './styles.css'
+import IMAGE from './assets/Pierres_shop.png'
 
 export const App = () => {
   return (
@@ -7,5 +7,5 @@ export const App = () => {
       <h1>React Typescript Webpack Starter Template</h1>
       <img src={IMAGE} alt="Pierre's Store" width="300" height="200" />
     </>
-  );
-};
+  )
+}
