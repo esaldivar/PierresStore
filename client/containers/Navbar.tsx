fb381529434cd5fc6b76031b5bb737bbd9812274
Eric Saldivar <Eric@samlas-mbp.lan>
2021-08-30 23:13:47 -0500

    Pierre icon added.  Going to start separating sass files

--- client/containers/Navbar.tsx
+++ client/containers/Navbar.tsx
@@ -1,9 +1,10 @@
+import './NavBar.sass';
 
 const NavBar = () => {
 	return (
 		<div className="navBar">
 			<ul>
-				<li>Icon</li>
+				<li><img className="pierreIcon" src="https://pierresstore.s3.us-east-2.amazonaws.com/Pierre.png"/></li>
 				<li>Favorites</li>
 				<li>In Season</li>
 				<li>Search Bar</li>
