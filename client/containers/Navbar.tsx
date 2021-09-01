8a93301103861cea98f2c62a2793a98134613180
Eric Saldivar <Eric@samlas-mbp.lan>
2021-08-31 23:55:33 -0500

    styled search bar

--- client/containers/Navbar.tsx
+++ client/containers/Navbar.tsx
@@ -1,4 +1,8 @@
 import './NavBar.sass';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+
 
 const NavBar = () => {
 	return (
@@ -13,8 +17,9 @@ const NavBar = () => {
 			<div>
 				<a href=""><h4>In Season</h4></a>
 			</div>
-			<div>
-				<input className="navSearch" type="search" name="search" placeholder="Search Pierre's Store" title="Search Stardew Valley Wiki [ctrl-option-f]"  id="searchInput" autoComplete="off"/>
+			<div className="search">
+				<FontAwesomeIcon className="searchIcon" icon={faSearch} size="xs"/>	
+				<input className="navSearch" type="search" name="search" placeholder="Search Pierre's Store" id="searchInput" autoComplete="off"/>	
 			</div>
 			<div>
 				<a href="https://sass-lang.com/guide"><img className="shoppingCartIcon"src="https://pierresstore.s3.us-east-2.amazonaws.com/Traveling_Cart.png"/></a>
