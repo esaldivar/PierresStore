7d1b32258da3fde9ec5ea05c7bd9cb490fb6d2df
Eric Saldivar <Eric@samlas-mbp.lan>
2021-08-31 22:26:30 -0500

    nav bar styled and set up as intended

--- client/containers/Navbar.tsx
+++ client/containers/Navbar.tsx
@@ -3,14 +3,23 @@ import './NavBar.sass';
 const NavBar = () => {
 	return (
 		<div className="navBar">
-			<ul>
-				<li><img className="pierreIcon" src="https://pierresstore.s3.us-east-2.amazonaws.com/Pierre.png"/></li>
-				<li>Favorites</li>
-				<li>In Season</li>
-				<li>Search Bar</li>
-				<li>User Icon</li>
-				<li><a href="https://sass-lang.com/guide"><img className="shoppingCartIcon"src="https://pierresstore.s3.us-east-2.amazonaws.com/Traveling_Cart.png"/></a></li>
-			</ul>
+			<div className="iconDiv">
+				<img className="pierreIcon" src="https://pierresstore.s3.us-east-2.amazonaws.com/Pierre.png"/>
+				<h4>Pierre's Store</h4>
+			</div>
+			<div>
+				<a href=""><h4>Favorites</h4></a>
+			</div>
+			<div>
+				<a href=""><h4>In Season</h4></a>
+			</div>
+			<div>
+				<input type="search" name="search" placeholder="Search Pierre's Store" title="Search Stardew Valley Wiki [ctrl-option-f]"  id="searchInput" autoComplete="off"/>
+			</div>
+			<div>
+				<a href="https://sass-lang.com/guide"><img className="shoppingCartIcon"src="https://pierresstore.s3.us-east-2.amazonaws.com/Traveling_Cart.png"/></a>
+			</div>
+				
 		</div>
 	)
 };
