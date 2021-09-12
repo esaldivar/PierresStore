fa4b8b07c46e2aed96e04253eb65cf6c971d6232
Eric Saldivar <Eric@samlas-mbp.lan>
2021-09-12 18:25:35 -0500

    able to add docker but having an issue with bcrypt

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const register = require('../controllers/authentication/register');
-const login = require('../controllers/authentication/login');
-const isAlreadyLoggedIn = require('../controllers/authentication/alreadyLoggedIn');
+// const register = require('../controllers/authentication/register');
+// const login = require('../controllers/authentication/login');
+// const isAlreadyLoggedIn = require('../controllers/authentication/alreadyLoggedIn');
 const retrieveInventory = require('../controllers/store/inventory');
 const seasonInfo = require('../controllers/session/season');
 
-router.post('/register', isAlreadyLoggedIn, register, (req, res) => {
-	return res.status(200).json({message: 'You successfully signed up!'});
-  });
+// router.post('/register', isAlreadyLoggedIn, register, (req, res) => {
+// 	return res.status(200).json({message: 'You successfully signed up!'});
+//   });
 
-router.post('/login', isAlreadyLoggedIn, login, (req, res) => {
-	return res.status(200).json({message: 'You successfully logged in!'});
-  });
+// router.post('/login', isAlreadyLoggedIn, login, (req, res) => {
+// 	return res.status(200).json({message: 'You successfully logged in!'});
+//   });
 
 router.get('/inventory', retrieveInventory, (req, res) => {
 	return res.status(200).send(res.inventory);
