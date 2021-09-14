86769547936bd81d926f447c957ad9929f08f803
eric saldivar <esaldivar1214@gmail.com>
2021-09-14 14:53:52 -0500

    trying to get mongoDB to play nice with heroku

--- server/controllers/session/season.js
+++ server/controllers/session/season.js
@@ -1,31 +1,31 @@
-// const db = require('../../models/storeModel');
+const db = require('../../models/storeModel');
 
-// const season = async (req, res, next) => {
+const season = async (req, res, next) => {
 
-// 	const date = new Date(); 
+	const date = new Date(); 
 
-// 	let season = '';
+	let season = '';
 
-// 	if(date.getMonth() > 1 && date.getMonth() < 5 ){
-// 		season = 'Spring';
-// 	} else if(date.getMonth() > 4 && date.getMonth() < 7){
-// 		season = 'Summer';
-// 	} else if(date.getMonth() > 6 && date.getMonth() < 9){
-// 		season = 'Fall';
-// 	} else {
-// 		season = 'Winter';
-// 	}
+	if(date.getMonth() > 1 && date.getMonth() < 5 ){
+		season = 'Spring';
+	} else if(date.getMonth() > 4 && date.getMonth() < 7){
+		season = 'Summer';
+	} else if(date.getMonth() > 6 && date.getMonth() < 9){
+		season = 'Fall';
+	} else {
+		season = 'Winter';
+	}
 
 
-// 	try {
-// 		const postQuery = await db.query(`SELECT * FROM products
-// 		WHERE '${season}'=ANY(season);`);
-// 		res.season = postQuery.rows;
-//         return next();
-//     } catch(err) {
-//         console.log('season error', err);
-//         return res.status(500).json({message: 'Could not retrieve inventory.'});
-//     }
-//   };
+	try {
+		const postQuery = await db.query(`SELECT * FROM products
+		WHERE '${season}'=ANY(season);`);
+		res.season = postQuery.rows;
+        return next();
+    } catch(err) {
+        console.log('season error', err);
+        return res.status(500).json({message: 'Could not retrieve inventory.'});
+    }
+  };
 
-//   module.exports = season;
+  module.exports = season;
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,7 +4,7 @@ const register = require('../controllers/authentication/register');
 const login = require('../controllers/authentication/login');
 const isAlreadyLoggedIn = require('../controllers/authentication/alreadyLoggedIn');
 const retrieveInventory = require('../controllers/store/inventory');
-// const seasonInfo = require('../controllers/session/season');
+const seasonInfo = require('../controllers/session/season');
 
 router.post('/register', isAlreadyLoggedIn, register, (req, res) => {
 	return res.status(200).json({message: 'You successfully signed up!'});
@@ -18,8 +18,8 @@ router.get('/inventory', retrieveInventory, (req, res) => {
 	return res.status(200).send(res.inventory);
 })
 
-// router.get('/season', seasonInfo, (req, res) => {
-// 	return res.status(200).send(res.season);
-// })
+router.get('/season', seasonInfo, (req, res) => {
+	return res.status(200).send(res.season);
+})
 
 module.exports = router;
